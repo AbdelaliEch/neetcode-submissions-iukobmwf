6ed6d171b99a0dff91014fab5b6633b7b9b4fbdd
AbdelaliEch <abdou.alaali10@gmail.com>
2026-05-03 06:58:59 +0100

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        windows = [s2[i: i + len(s1)] for i in range(len(s2) - len(s1) + 1)]
+        print(windows)
+        for window in windows:
+            if Counter(window) == Counter(s1):
+                return True
+        return False
+
+            
+            
