7a2ebdfd0a6e4e14573eb9c35cc7d6eb3f341c99
AbdelaliEch <abdou.alaali10@gmail.com>
2026-06-04 06:46:19 +0100

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:        
+        my_dict = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for lt in s:
+                count[ord(lt.lower()) - ord('a')] += 1
+            my_dict[tuple(count)].append(s)
+
+        return list(my_dict.values())
+        
+# complexity: O(n*m) O(n)
