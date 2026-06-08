08eec2628f7d422eb1e9afb3fbad98e5be718fc0
AbdelaliEch <abdou.alaali10@gmail.com>
2026-06-08 07:26:56 +0100

    Add: three-integer-sum - submission-25

---
+++ Data Structures & Algorithms/three-integer-sum/submission-25.py
@@ -0,0 +1,40 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums = sorted(nums)
+        visited_nums = set()
+
+        for i in range(len(nums)):
+            if nums[i] in visited_nums:
+                continue
+            if nums[i] > 0:
+                break
+            visited_nums.add(nums[i])
+            target = -1 * nums[i]
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                addition = nums[j] + nums[k]
+                if addition > target:
+                    k -= 1
+                elif addition < target:
+                    j += 1
+                else:
+                    res.append([nums[i], nums[j], nums[k]])
+                    while j < k and nums[k] + nums[j] + nums[i] == 0:
+                            k-=1
+                        
+        return res
+
+# complexity: O(n**2) O(n)
+# [-2,0,1,1,2]
+
+
+
+
+# for the result of three numbers to be 0, one of them must be negative, so the loop must stop when
+# we reach a strictly positive number
+
+# [-4, -1, 0, 1, 2]
+        
+# nums[i] + nums[j] + nums[k] = 0
+# nums[i] + nums[j] = - nums[k]
