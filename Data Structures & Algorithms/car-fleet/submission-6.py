265660c805c5e239b9db16b57962e354c51053cf
AbdelaliEch <abdou.alaali10@gmail.com>
2026-06-10 06:43:04 +0100

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,26 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        position_speed = [(position[i], speed[i]) for i in range(len(position))]
+        position_speed.sort(key=lambda x: x[0], reverse=True)
+        
+        count = 0
+        stack = []
+        for pos, speed in position_speed:
+            d = (target - pos) / speed
+            if not stack:
+                stack.append((pos, speed, d))
+            else:
+                if d > stack[-1][2]:
+                    stack.append((pos, speed, d))
+        return len(stack)
+
+# v = d/t
+# t = (10 - position) / speed
+# [(7, 1), (4, 2), (1, 2), (0, 1)]
+
+
+# number of iterations each car will take to reach target is:
+# x = (target - position) / speed
+
+
+# target = 10, position = [4,1,0,7], speed = [2,2,1,1]
