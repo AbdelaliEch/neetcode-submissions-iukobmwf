7808f739df7f3921c106048e57c9bfb0ac19d2df
AbdelaliEch <abdou.alaali10@gmail.com>
2026-06-10 07:25:35 +0100

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = right = max_profit = 0
+
+        current_min = prices[left]
+
+        while right < len(prices):
+            if prices[right] < prices[left]:
+                left = right
+            max_profit = max(prices[right] - prices[left], max_profit)
+            right += 1
+
+
+            # if prices[left] < current_min:
+            #     current_min = prices[left]
+            # else:
+            #     left += 1
+            # max_profit = max(prices[right] - prices[left], max_profit)
+            # right += 1
+
+        return max_profit
+        
+
+# left needs to always track current min
+# right needs to just move and calculate
+
+
+# to get the max profit, we need to buy low and sell high
+# so buy at minimum
+# so why not iterate thro
+# [10,1,5,6,7,1]  
