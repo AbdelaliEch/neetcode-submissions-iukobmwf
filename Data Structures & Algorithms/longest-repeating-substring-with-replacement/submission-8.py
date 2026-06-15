4c437486d938b71b27bd062941b492acbebdccc8
AbdelaliEch <abdou.alaali10@gmail.com>
2026-06-15 14:26:01 +0100

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.py
@@ -0,0 +1,39 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        freq_count = {}
+
+        left = 0
+        max_freq = 0
+        for right in range(len(s)):
+            freq_count[s[right]] = 1 + freq_count.get(s[right], 0)
+            max_freq = max(max_freq, freq_count[s[right]])
+
+            while (right - left + 1) - max_freq > k:
+                freq_count[s[left]] -= 1
+                left+=1
+            
+            res = max(res, right - left + 1)
+    
+        return res
+            
+
+
+# complexity: O(n) O(m)
+
+
+
+# ok so we'll use a variable-size sliding window
+# when is it valid
+# and when is it invalid
+
+# the window is valid as long as we can make enough changes in the current 
+# substring
+# and how will we make changes???
+# in each substring, we need to count the most frequent letter, and change
+# the rest to the letters to it.
+# so the validity of the window will be:
+# if k >= len(substring) - count(mostFrequentLetter)
+# else, invalid
+
+
